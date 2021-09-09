1cb8450a0cf76c38c1a658405fbaf82b3461160a
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2021-09-09 16:34:55 -0300

    Date

--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,80 @@ const profile = {
   'monthly-budget': 3000,
   'days-per-week': 5,
   'hours-per-day': 5,
-  'vacation-per-year': 4
+  'vacation-per-year': 4,
+  'value-hour': 75
 }
 
-const jobs = []
+const jobs = [
+  {
+    id: 1,
+    name: 'Pizzaria Gabriel',
+    'daily-hours': 2,
+    'total-hours': 1,
+    createdAt: Date.now(),
+    budget: 4000,
+    remaining: 5,
+    status: 'progress'
+  },
+  {
+    id: 2,
+    name: 'OneTwo Project',
+    'daily-hours': 3,
+    'total-hours': 50,
+    createdAt: Date.now(),
+    budget: 4500,
+    remaining: 6,
+    status: 'done'
+  }
+]
 
-routes.get('/', (req, res) => res.render(views + '/index'))
+function remainingDays(job) {
+  const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
+
+    const createdDate = new Date(job.createdAt)
+    const dueDay = createdDate.getDate() + Number(remainingDays)
+    const dueDateInMs = createdDate.setDate(dueDay)
+
+    const timeDiffInMs = dueDateInMs - Date.now()
+
+    const dayInMs = 1000 * 60 * 60 * 24
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+    return dayDiff
+}
+
+routes.get('/', (req, res) => {
+//ajustes no job
+  const updatedJobs = jobs.map((job) => {
+
+    const remaining = remainingDays(job)
+
+    const status = remaining <= 0 ? 'done' : 'progress'
+
+    return {
+      ...job,
+      remaining,
+      status,
+      budget: profile['value-hour'] * job['total-hours']
+    }
+  })
+  return res.render(views + 'index', { jobs: updatedJobs })
+})
 
 routes.get('/job', (req, res) => res.render(views + '/job'))
 routes.post('/job', (req, res) => {
   // { name: 'GoBarber', 'daily-hours': '8', 'total-hours': '2' }
-  jobs.push(req.body)
+  const lastId = jobs[jobs.length - 1]?.id || 1;
+
+  jobs.push(
+    {
+      id: lastId + 1,
+      name: req.body.name,
+      'daily-hours': req.body['daily-hours'],
+      'total-hours': req.body['total-hours'],
+      createdAt: Date.now()
+    }
+  )
   return res.redirect('/')
 })
 
